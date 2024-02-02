fcd1d92e0551849646d806243818dc2cc0431c07
U-Gabriel <gabrielarie2000@gmail.com>
2024-02-02 01:36:37 +0100

    add creation delivery with api connexion

--- IOS/EFD/EFD/BossViewController/DeliveryMan/CreateDeliveryManViewController.swift
+++ IOS/EFD/EFD/BossViewController/DeliveryMan/CreateDeliveryManViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class CreateDeliveryManViewController: UIViewController {
-
+    
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldConfirmPassword: UITextField!
+    
+    @IBOutlet weak var labelStatus: UILabel!
+    
+    @IBOutlet weak var buttonToCreateD: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonToCreateD.layer.cornerRadius = 8.00 // Pour obtenir les coins arrondis
+        
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func goToCreateD(_ sender: Any) {
+        if textFieldUsername.text != "" && textFieldEmail.text != "" && textFieldPassword.text != "" && textFieldConfirmPassword.text != ""{
+            
+            if textFieldPassword.text == textFieldConfirmPassword.text{
+                
+                EFDWebServices.addDelivery(username: textFieldUsername.text!, email: textFieldEmail.text!, password: textFieldPassword.text!){ err, success in
+                        guard (success != nil) else {
+                            return
+                        }
+                        DispatchQueue.main.async {
+                            if success == true {
+                                self.labelStatus.text = "Le livreur à bien été créé."
+                                self.labelStatus.textColor = UIColor.green
+                            }else{
+                                self.labelStatus.text = "Cette utilisateur existe déjà"
+                                self.labelStatus.textColor = UIColor.red
+                            }
+                        }
+                    }
+            }else {
+                self.labelStatus.text = "Les mots de passe ne correspondent pas."
+                self.labelStatus.textColor = UIColor.red
+            }
+        } else {
+            self.labelStatus.text = "Tous les champs doivent être remplis."
+            self.labelStatus.textColor = UIColor.red
+        }
     }
-    */
+    
+   
 
 }
