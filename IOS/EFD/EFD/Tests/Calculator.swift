fde5b08621a58bee4f56dc5be69a008bb51e409e
bingchillin <92374981+bingchillin@users.noreply.github.com>
2024-03-01 22:41:12 +0100

    Added small unit test

---
+++ IOS/EFD/EFD/Tests/Calculator.swift
@@ -0,0 +1,14 @@
+//
+//  Calculator.swift
+//  EFD
+//
+//  Created by Mohamed El Fakharany on 01/03/2024.
+//
+
+import Foundation
+
+class Calculator {
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ IOS/EFD/EFD/Tests/UnitTests.swift
@@ -0,0 +1,29 @@
+//
+//  UnitTests.swift
+//  EFDTests
+//
+//  Created by Mohamed El Fakharany on 01/03/2024.
+//
+
+import XCTest
+
+@testable import EFD
+
+class CalculatorTests: XCTestCase {
+    var calculator: Calculator!
+
+    override func setUp() {
+        super.setUp()
+        calculator = Calculator()
+    }
+
+    override func tearDown() {
+        calculator = nil
+        super.tearDown()
+    }
+
+    func testAddition() {
+        XCTAssertEqual(calculator.add(2, 3), 5, "Adding 2 and 3 should result in 5")
+    }
+}
+
